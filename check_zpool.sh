87311793ad59f30f8b718a39a39be605a638a60e
Søren Klintrup <soren.klintrup@lego.com>
2023-12-01 11:28:49 +0100

    update with latest shellcheck findings

--- check_zpool.sh
+++ check_zpool.sh
@@ -2,7 +2,7 @@
 # NRPE check for zpool
 # Written by: Søren Klintrup <github at klintrup.dk>
 # Get your copy from https://github.com/Klintrup/check_zpool
-# version 1.2
+# version 1.x
 
 PATH="/sbin:/bin:/usr/sbin:/usr/bin"
 if [ -x "/sbin/zpool" ]
@@ -19,12 +19,12 @@ unset ERR
 for DEVICE in ${DEVICES}
 do
  DEVICESTRING="$(zpool list -H -o health ${DEVICE})"
- if [ "$(echo ${DEVICESTRING}|tr [:upper:] [:lower:]|sed -Ee 's/.*(degraded|faulted|offline|online|removed|unavail).*/\1/')" = "" ]
+ if [ "$(echo "${DEVICESTRING}"|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(degraded|faulted|offline|online|removed|unavail).*/\1/')" = "" ]
  then
   ERRORSTRING="${ERRORSTRING} / ${DEVICE}: unknown state"
   if ! [ "${ERR}" = 2 ];then ERR=3;fi
  else
-  case $(echo ${DEVICESTRING}|tr [:upper:] [:lower:]|sed -Ee 's/.*(degraded|faulted|offline|online|removed|unavail).*/\1/') in
+  case $(echo "${DEVICESTRING}"|tr '[:upper:]' '[:lower:]'|sed -Ee 's/.*(degraded|faulted|offline|online|removed|unavail).*/\1/') in
    degraded)
      ERR=2
      ERRORSTRING="${ERRORSTRING} / ${DEVICE}: DEGRADED"
@@ -45,10 +45,6 @@ do
      ERR=2
      ERRORSTRING="${ERRORSTRING} / ${DEVICE}: UNAVAIL"
     ;;
-   faulted)
-    ERR=3
-    ERRORSTRING="${ERRORSTRING} / ${DEVICE}: FAULTED"
-    ;;
    online)
     OKSTRING="${OKSTRING} / ${DEVICE}: online"
     ;;
@@ -59,10 +55,10 @@ if [ "${1}" ]
 then
  if [ "${ERRORSTRING}" ]
  then
-  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E ${*}
+  echo "${ERRORSTRING} ${OKSTRING}"|sed s/"^\/ "//|mail -s "$(hostname -s): ${0} reports errors" -E "${*}"
  fi
 else
- if [ "${ERRORSTRING}" -o "${OKSTRING}" ]
+ if [ "${ERRORSTRING}" ] || [ "${OKSTRING}" ]
  then
   echo "${ERRORSTRING} ${OKSTRING}"|sed -E s/"^[[:blank:]]{1,}\/ "//
   exit ${ERR}
